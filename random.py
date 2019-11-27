fd6c4a68ae92e254647b5f007ea4b8db0375f126
Sampath Kumar <sampathk@twitter.com>
2019-11-26 22:21:53 -0800

    Learning to Func

---
+++ random.py
@@ -0,0 +1,22 @@
+#This script it to generate two random numbers and add them
+#and compare respose from user to see if he is right
+
+# import random
+# for i in range(1,100):
+#     num1 = random.randint(1, 100)
+#     num2 = random.randint(1, 100)
+#     print str(num1) + str num2
+
+import random
+
+num1 = random.randint(1, 100)
+num2 = random.randint(1, 100)
+    
+print ("What is " + str(num1) + " + " + str(num2))
+
+answer = num1 + num2
+
+yourAnswer = int(input())
+
+if answer == yourAnswer:
+    print ("Awesome")
